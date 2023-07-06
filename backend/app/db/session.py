e1123aef54d641c2f7f442e1a31755f2dc3596a4
duduru <hgalytoby@gmail.com>
2023-07-06 23:59:39 +0800

    update.

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -6,20 +6,12 @@ from app.core.config import settings
 from debug_toolbar.panels.sqlalchemy import SQLAlchemyPanel as BasePanel
 from fastapi import Request
 
-DB_POOL_SIZE = 83
-WEB_CONCURRENCY = 9
-POOL_SIZE = max(DB_POOL_SIZE // WEB_CONCURRENCY, 10)
-
 engine = create_async_engine(
     settings.pg.url,
     echo=False,
     pool_pre_ping=True,
-    pool_size=POOL_SIZE,
+    pool_size=5,
     max_overflow=64,
-    connect_args={
-        'prepared_statement_cache_size': 0,
-        'statement_cache_size': 0,
-    }
 )
 async_session_maker = sessionmaker(
     autocommit=False,
